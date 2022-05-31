20e1af50d871fedd847d82b45b581578c993c44e
ODZERAWPROGRAMOWANIU <hubertm999@gmail.com>
2022-05-31 13:02:23 +0200

    Adding a headers to respository

---
+++ headers.h
@@ -0,0 +1,14 @@
+#ifndef _HEADERS_H
+#define _HEADERS_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+using namespace std;
+void checkAttendance();
+void addStudent();
+void allStudentsWithGrades();
+void calculateAverage();
+void showAttendance();
+void drawStudent();
+#endif
